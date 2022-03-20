108ca329c210b1b7f2dd1ee8ce45d29a6c1a878c
asachiyigor <asachiyigor@gmail.com>
2022-03-20 18:30:58 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,17 @@ public class Main {
         int income = s.nextInt();
         System.out.println("--------------------------------");
 
-        int result = IsIncomeMoreThan1000(income) ? (startAmount + income + HowManyBonusRubles(income)) : startAmount + income;
+        int result = isIncomeMoreThan1000(income) ? (startAmount + income + howManyBonusRubles(income)) : startAmount + income;
 
-        System.out.println(IsIncomeMoreThan1000(income) ? "Количествоо бонусных рублей: " + HowManyBonusRubles(income)+"\nИтоговый счёт: " + result : "Бонусов нет\nИтоговый счёт: " + result);
+        System.out.println(isIncomeMoreThan1000(income) ? "Количествоо бонусных рублей: " + howManyBonusRubles(income)+"\nИтоговый счёт: " + result : "Бонусов нет\nИтоговый счёт: " + result);
         System.out.println("--------------------------------");
     }
 
-    public static boolean IsIncomeMoreThan1000(int income) {
+    public static boolean isIncomeMoreThan1000(int income) {
         return (income > 1000);
     }
 
-    public static int HowManyBonusRubles(int income) {
-        return IsIncomeMoreThan1000(income) ? income / 100 : 0;
+    public static int howManyBonusRubles(int income) {
+        return isIncomeMoreThan1000(income) ? income / 100 : 0;
     }
 }
